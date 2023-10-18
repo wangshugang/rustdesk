142451f928c39bb9d4bd3a11f4b7e495e3eadf18
shugang.wang <wsgdhy@126.com>
2023-10-18 13:31:27 +0800

    修改RENDEZVOUS_SERVERS默认地址

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -90,7 +90,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
+// pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
+pub const RENDEZVOUS_SERVERS: &[&str] = &["10.43.33.208"];
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
